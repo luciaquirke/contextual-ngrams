51ed0ed957d674762a58005e97cd18e081e647c6
Lucia Quirke <luciarosequirke@gmail.com>
2023-10-02 08:10:42 +0000

    Update axis labels and legend

--- contextual_ngram_formation_viz.py
+++ contextual_ngram_formation_viz.py
@@ -5,12 +5,13 @@ import gzip
 import argparse
 import plotly.express as px
 import pandas as pd
+import plotly.graph_objects as go
 
 
 def get_good_mcc_neurons(probe_df: pd.DataFrame):
     neurons = probe_df[(probe_df["MCC"] > 0.85) & (probe_df["MeanGermanActivation"]>probe_df["MeanNonGermanActivation"])][["NeuronLabel", "MCC"]].copy()
     neurons = neurons.sort_values(by="MCC", ascending=False)
-    good_neurons = neurons["NeuronLabel"].unique()[:10]
+    good_neurons = neurons["NeuronLabel"].unique()
 
     return good_neurons
 
@@ -43,20 +44,22 @@ def produce_images(model_name: str, save_data_path: Path, save_image_path: Path)
     ablation_df.sort_values(by=["Checkpoint", "Label"], inplace=True)
 
     L3N669_df = ablation_df[ablation_df["Label"] == "L3N669"]
+    L3N669_df['Loss Increase'] = L3N669_df['AblationIncrease']
     non_L3N669_df = ablation_df[(ablation_df["Label"] != "L3N669")]
+    non_L3N669_df['Loss Increase'] = non_L3N669_df['AblationIncrease']
     non_L3N669_df['Label'] = 'Other neurons with MCC > 0.85'
-    
+
     percentiles = [0.25, 0.5, 0.75]
-    grouped = non_L3N669_df.groupby('Checkpoint')['AblationIncrease'].describe(percentiles=percentiles).reset_index()
+    grouped = non_L3N669_df.groupby('Checkpoint')['Loss Increase'].describe(percentiles=percentiles).reset_index()
 
     fig = go.Figure()
 
     fig.add_trace(go.Scatter(x=grouped['Checkpoint'], y=grouped['25%'], fill=None, mode='lines', line_color='rgba(31, 119, 180, 0.4)', showlegend=False))
     fig.add_trace(go.Scatter(x=grouped['Checkpoint'], y=grouped['75%'], fill='tonexty', fillcolor='rgba(31, 119, 180, 0.2)', line_color='rgba(31, 119, 180, 0.4)', showlegend=False))
-    fig.add_trace(go.Scatter(x=grouped['Checkpoint'], y=grouped['50%'], mode='lines', line=dict(color='#1F77B4', width=2), name="Other Context Neurons (Median)"))
-    fig.add_trace(go.Line(x=L3N669_df['Checkpoint'], y=L3N669_df['AblationIncrease'], mode='lines', line=dict(color='#FF7F0E', width=2), name="L3N669"))
+    fig.add_trace(go.Scatter(x=grouped['Checkpoint'], y=grouped['50%'], mode='lines', line=dict(color='#1F77B4', width=2), name="Other Context<br>Neurons (Median)"))
+    fig.add_trace(go.Line(x=L3N669_df['Checkpoint'], y=L3N669_df['Loss Increase'], mode='lines', line=dict(color='#FF7F0E', width=2), name="L3N669"))
 
-    fig.update_layout(title="Loss Increase on German Text when Ablating Context Neurons", xaxis_title="Checkpoint", yaxis_title="AblationIncrease")
+    fig.update_layout(title="Loss Increase on German Text when Ablating Context Neurons", xaxis_title="Checkpoint", yaxis_title="Loss Increase")
 
     fig.write_image(save_image_path.joinpath("ablation_increase_german_text.png"), width=2000)
 
